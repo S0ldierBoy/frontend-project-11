5c5b97a27e8ba1ea27e8a547a876c43d9a2033bf
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-02 23:03:00 +0200

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ['node_modules', 'dist'],
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
 ];
