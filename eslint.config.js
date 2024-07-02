9dc0280bcd1c1ba40cdb459374335ea964d84c0e
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-02 18:29:16 +0200

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
@@ -8,5 +9,5 @@ export default [
   { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
-  pluginReactConfig,
+  ...fixupConfigRules(pluginReactConfig),
 ];
