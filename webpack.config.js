70f41cf75d5e7268f1e8f10ca046fa75df675fc7
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-03 05:17:22 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 import process from 'process';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
-import { __dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
