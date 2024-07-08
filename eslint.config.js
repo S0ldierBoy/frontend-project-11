60f048661a72d845b70b59af85728642752dfda2
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-08 15:05:53 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js';
 
 export default [
   {
-    ignores: ['**/node_modules/', '.dist/'],
+    ignores: ['**/node_modules/', 'index.js'],
     languageOptions: { globals: globals.browser },
   },
   pluginJs.configs.recommended,
