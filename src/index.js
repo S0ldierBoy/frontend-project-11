8b13c51fc6d6f7343fd5d901346616919a997f0e
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-07-09 10:53:20 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import '../src/styles.css';
-console.log('Hello World!');
+import * as yup from 'yup';
+
+const button = document.querySelector('[type="submit"]');
+button.addEventListener('click', () => {
+  alert('Кнопка была нажата!');
+});
+
+// const schema = yup.object().shape({
+//   name: yup.string().trim().required(),
+//   email: yup.string().required('email must be a valid email').email(),
+//   password: yup.string().required().min(6),
+//   passwordConfirmation: yup
+//     .string()
+//     .required('password confirmation is a required field')
+//     .oneOf(
+//       [yup.ref('password'), null],
+//       'password confirmation does not match to password'
+//     ),
+// });
