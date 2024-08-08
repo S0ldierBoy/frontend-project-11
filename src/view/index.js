92a06ea10b0cf6f495b16287258ce208444c6b05
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-08-08 19:21:33 +0200

    fix

--- src/view/index.js
+++ src/view/index.js
@@ -3,9 +3,7 @@ import renderPosts from './renderPosts.js';
 
 const feedView = (state, i18nextInstance) => {
   const { feeds, posts } = state;
-
   renderFeed(feeds, i18nextInstance);
-
   renderPosts(posts, i18nextInstance);
 };
 
