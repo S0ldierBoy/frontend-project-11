a2274ae330ef8e8ca75cde6b724af4f97b4e85bd
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-08-09 09:45:28 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -16,27 +16,3 @@ const fetchRss = (url) => {
 };
 
 export default fetchRss;
-
-// import axios from 'axios';
-
-// const fetchRss = (url) => {
-//   const allOriginsUrl = `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`;
-
-//   return axios
-//     .get(allOriginsUrl, {
-//       headers: {
-//         'Cache-Control': 'no-cache',
-//         Pragma: 'no-cache',
-//       },
-//     })
-//     .then((response) => response.data)
-//     .catch((error) => {
-//       if (error.response || error.request) {
-//         throw new Error('errors.netError2'); // Ошибка сервера, например 404 или 500
-//       } else {
-//         throw new Error('errors.netError1'); // Ошибка запроса
-//       }
-//     });
-// };
-
-// export default fetchRss;
