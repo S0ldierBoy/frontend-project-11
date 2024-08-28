58d979f8b733864ae761c5af78edb71fddc9dd00
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-08-28 21:01:14 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -3,18 +3,13 @@ import domParser from './domParser.js';
 
 const fetchRss = (url) =>
   axios
-    .get(
-      `https://allorigins.hexlet.app/get?url=${encodeURIComponent(
-        url
-      )}&disableCache=true`,
-      {
-        timeout: 10000, // Устанавливаем таймаут на 5 секунд
-      }
-    )
+    .get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`, {
+      timeout: 10000,
+    })
     .then((response) => response.data)
     .catch((error) => {
       if (error.response || error.request) {
-        throw new Error('errors.netError1'); // Ошибка сервера,  например 404 или 500
+        throw new Error('errors.netError1'); // Ошибка сервера, например 404 или 500
       } else {
         throw new Error('errors.netError2'); // Ошибка запроса
       }
@@ -25,10 +20,9 @@ const checkForUpdates = (state, watchedState) => {
     fetchRss(url)
       .then((data) => domParser(data.contents))
       .then((parsedData) => {
-        const newPosts = parsedData.posts.filter(
-          (post) =>
-            !state.posts.some((existingPost) => existingPost.link === post.link)
-        );
+        const newPosts = parsedData.posts.filter((post) => !state.posts.some(
+          (existingPost) => existingPost.link === post.link,
+        ));
 
         if (newPosts.length > 0) {
           watchedState.posts.unshift(...newPosts);
@@ -36,7 +30,7 @@ const checkForUpdates = (state, watchedState) => {
       })
       .catch((error) => {
         console.error(`Error fetching RSS feed from ${url}:`, error.message);
-      })
+      }),
   );
 
   Promise.all(updatePromises).finally(() => {
