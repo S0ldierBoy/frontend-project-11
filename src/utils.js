588379b57539d563c65f8f3e1ec715b881a87fba
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-08-28 17:39:59 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 import domParser from './domParser.js';
 
-const fetchRss = (url) => {
+const fetchRss = (url, watchedState) => {
   return axios
     .get(
-      `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`
+      `https://allorigins.hexlet.app/get?url=${encodeURIComponent(
+        url
+      )}&disableCache=true`
     )
     .then((response) => response.data)
     .catch((error) => {
       if (error.response || error.request) {
-        throw new Error('errors.netError2'); // Ошибка сервера,  например 404 или 500
+        watchedState.error = 'Ошибка сервера. Проверьте URL или подключение.';
+        throw new Error('errors.netError2');
       } else {
-        throw new Error('errors.netError1'); // Ошибка запроса
+        watchedState.error = 'Ошибка сети. Проверьте ваше интернет-соединение.';
+        throw new Error('errors.netError1');
       }
     });
 };
