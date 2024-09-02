212cffbb6f46ee1431c965cbacc65427cfd7bf19
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-09-02 23:58:35 +0200

    fix

--- src/view/watchState.js
+++ src/view/watchState.js
@@ -2,7 +2,7 @@ import onChange from 'on-change';
 import renderInput from './renderInput.js';
 import feedView from './index.js';
 
-const createWatchState = (state, elements, i18nextInstance) =>
+const createWatchState = (state, elements, i18nextInstance) => {
   onChange(state, () => {
     switch (state.load) {
       case 'process':
@@ -16,5 +16,6 @@ const createWatchState = (state, elements, i18nextInstance) =>
         break;
     }
   });
+};
 
 export default createWatchState;
