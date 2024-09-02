e5f0fd352617ead967a05eb011b74b32a8e957b0
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-09-03 00:06:52 +0200

    fix

--- src/view/watchState.js
+++ src/view/watchState.js
@@ -4,14 +4,10 @@ import feedView from './index.js';
 
 const createWatchState = (state, elements, i18nextInstance) =>
   onChange(state, () => {
-    switch (state.load) {
-      case 'process':
-        renderInput(elements, state, i18nextInstance);
-        feedView(state);
-        break;
-      case 'error':
-        renderInput(elements, state, i18nextInstance);
-        break;
+    renderInput(elements, state, i18nextInstance);
+
+    if (state.load === 'process') {
+      feedView(state);
     }
   });
 
