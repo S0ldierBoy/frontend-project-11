c4a747bada650043cbc0741a5dab3115e46a97a0
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-09-02 23:54:10 +0200

    fix

--- src/view/watchState.js
+++ src/view/watchState.js
@@ -2,16 +2,17 @@ import onChange from 'on-change';
 import renderInput from './renderInput.js';
 import feedView from './index.js';
 
-const createWatchState = (state, elements, i18nextInstance) => onChange(state, () => {
+const createWatchState = (state, elements, i18nextInstance) =>
+  onChange(state, () => {
     switch (state.load) {
       case 'process':
         renderInput(elements, state, i18nextInstance);
         feedView(state);
         break;
       case 'error':
-        renderInput(elements, state, i18nextInstance);
         break;
       default:
+        renderInput(elements, state, i18nextInstance);
         break; // Убрана ненужная команда return
     }
   });
