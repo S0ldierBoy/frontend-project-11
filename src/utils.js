2ee4d8b4b4be4544f92321bdec34f871a2225020
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-09-03 02:18:42 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -19,30 +19,31 @@ const assignIdsToPosts = (data, url) => {
   return feedWithUrl;
 };
 
-const fetchRss = (url) => axios
-  .get(
-    `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`,
-    {
-      timeout: 10000,
-    },
-  )
-  .then((response) => response.data) // Получаем документ
-  .catch((error) => {
-    if (error.response || error.request) {
-      throw new Error('errors.netError1'); // Ошибка сервера, например 404 или 500
-    }
-    throw new Error('errors.netError2'); // Ошибка запроса
-  });
+const fetchRss = (url) =>
+  axios
+    .get(
+      `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`,
+      {
+        timeout: 10000,
+      },
+    )
+    .then((response) => response.data) // Получаем документ
+    .catch((error) => {
+      if (error.response || error.request) {
+        throw new Error('errors.netError1'); // Ошибка сервера, например 404 или 500
+      }
+      throw new Error('errors.netError2'); // Ошибка запроса
+    });
 
 const checkForUpdates = (state, watchedState) => {
-  const updatePromises = Object.values(state.feeds).map((feed) => fetchRss(feed.url)
-    .then((data) => domParser(data.contents))
-    .then((parsedData) => {
+  const updatePromises = Object.values(state.feeds).map((feed) =>
+    fetchRss(feed.url)
+      .then((data) => domParser(data.contents))
+      .then((parsedData) => {
         // Проверяем новые посты, которых нет в текущем фиде
-      const newPosts = parsedData.posts.filter(
+        const newPosts = parsedData.posts.filter(
           (post) =>
-            !feed.posts.some((existingPost) => existingPost.link === post.link)
-        );
+            !feed.posts.some((existingPost) => existingPost.link === post.link));
 
         if (newPosts.length > 0) {
           // Добавляем новые посты в начало массива
@@ -51,16 +52,15 @@ const checkForUpdates = (state, watchedState) => {
             posts: [...newPosts, ...feed.posts],
           };
           // Обновляем фид в watchedState
-          Object.assign(watchedState.feeds, { [feed.id]: updatedFeed });
+          watchedState.feeds[feed.id] = updatedFeed;
         }
       })
       .catch((error) => {
         console.error(
           `Error fetching RSS feed from ${feed.url}:`,
-          error.message
+          error.message,
         );
-      })
-  );
+      }));
 
   Promise.all(updatePromises).finally(() => {
     setTimeout(() => checkForUpdates(state, watchedState), 5000);
