a7f1dce926550e4b98d2c9d59f33a9427782a74c
S0ldierBoy <legendry.sh0wtime@gmail.com>
2024-09-17 16:14:18 +0200

    fix

--- src/utils.js
+++ src/utils.js
@@ -2,30 +2,32 @@ import _ from 'lodash';
 import axios from 'axios';
 import domParser from './domParser.js';
 
-export const fetchRss = (url) =>
+export const fetchRss = (url) => (
   axios.get(
-      `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`,
-      {
-        timeout: 10000,
-      })
+    `https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`,
+    {
+      timeout: 10000,
+    },
+  )
     .then((response) => response.data)
     .catch((error) => {
       if (error.response || error.request) {
         throw new Error('errors.networkError');
       }
       throw new Error('errors.serverError');
-    });
+    })
+);
 
 export const checkForUpdates = (watchedState) => {
-  const updatePromises = watchedState.feeds.map((feed) =>
+  const updatePromises = watchedState.feeds.map((feed) => (
     fetchRss(feed.url)
       .then((data) => {
         const parsedData = domParser(data.contents);
-        const newPosts = parsedData.posts.filter(
-          (post) =>
-            !watchedState.posts.some(
-              (existingPost) => existingPost.link === post.link
-            ));
+        const newPosts = parsedData.posts.filter((post) => (
+          !watchedState.posts.some(
+            (existingPost) => existingPost.link === post.link,
+          )
+        ));
 
         const newPostsWithIds = newPosts.map((post) => ({
           ...post,
@@ -33,12 +35,13 @@ export const checkForUpdates = (watchedState) => {
           feedId: feed.id,
         }));
 
+        /* eslint-disable-next-line no-param-reassign */
         watchedState.posts = [...newPostsWithIds, ...watchedState.posts];
       })
       .catch((error) => {
         console.error(`errors.rssFetchError ${feed.url}:`, error.message);
       })
-  );
+  ));
 
   Promise.all(updatePromises).finally(() => {
     setTimeout(() => checkForUpdates(watchedState), 5000);
